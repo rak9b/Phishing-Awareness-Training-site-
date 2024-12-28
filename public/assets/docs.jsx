f8096b77a499ceb56cba90454a1e46b7f1cd89a1
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-28 22:20:02 +0600

    Create docs.jsx

---
+++ public/assets/docs.jsx
@@ -0,0 +1,13 @@
+const DownloadResources = () => (
+  <div className="text-center mt-8">
+    <a
+      href="/assets/docs/ceh-handbook.pdf"
+      download
+      className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+    >
+      Download CEH Handbook
+    </a>
+  </div>
+);
+
+export default DownloadResources;
