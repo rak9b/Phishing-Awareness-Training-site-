f648889955ff6c2705d4c31bf8cb0e4261a38e3f
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-28 18:42:14 +0600

    Create Icons.jsx

---
+++ src/ components /ui/Icons.jsx
@@ -0,0 +1,23 @@
+import {
+  Activity,
+  Shield,
+  AlertTriangle,
+  Terminal,
+  Book,
+  Target,
+  Lock,
+  Globe,
+} from 'lucide-react';
+
+const Icons = {
+  Activity,
+  Shield,
+  AlertTriangle,
+  Terminal,
+  Book,
+  Target,
+  Lock,
+  Globe,
+};
+
+export default Icons;
