35a001eed12bf2af0d74aaf0bebec6055e1156d3
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-28 22:09:41 +0600

    Create  Icons.jsx

---
+++ public/assets/Icons.jsx
@@ -0,0 +1,17 @@
+import ShieldIcon from '/assets/icons/shield.svg';
+import TargetIcon from '/assets/icons/target.svg';
+import ActivityIcon from '/assets/icons/activity.svg';
+import TerminalIcon from '/assets/icons/terminal.svg';
+import LockIcon from '/assets/icons/lock.svg';
+import GlobeIcon from '/assets/icons/globe.svg';
+
+const Icons = {
+  Shield: ShieldIcon,
+  Target: TargetIcon,
+  Activity: ActivityIcon,
+  Terminal: TerminalIcon,
+  Lock: LockIcon,
+  Globe: GlobeIcon,
+};
+
+export default Icons;
